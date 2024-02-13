5719f9925c213f1a65f14b5abd7c731fe1bd0375
gugomea <91557704+gugomea@users.noreply.github.com>
2024-02-13 17:43:13 +0100

    Update init.lua

--- lua/guillermo/init.lua
+++ lua/guillermo/init.lua
@@ -16,24 +16,25 @@ local ensure_packer = function()
   return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
   
-        use {
-          'nvim-telescope/telescope.nvim', tag = '0.1.1',
-          -- or                            , branch = '0.1.x',
-          requires = {
-              {'nvim-lua/plenary.nvim'},
-              { "nvim-telescope/telescope-live-grep-args.nvim" },
-          },
-          config = function()
-              require("telescope").load_extension("live_grep_args")
-          end
-      }
-  
-      use { "ellisonleao/gruvbox.nvim" }
-  
-      use {
-          'nvim-treesitter/nvim-treesitter',
-          run = ':TSUpdate'
-      }
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.5',
+		-- or                            , branch = '0.1.x',
+		requires = {
+			{'nvim-lua/plenary.nvim'},
+			{ "nvim-telescope/telescope-live-grep-args.nvim" },
+		},
+
+		config = function()
+			require("telescope").load_extension("live_grep_args")
+		end
+	}
+
+	use { "ellisonleao/gruvbox.nvim" }
+
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = ':TSUpdate'
+	}
   
       use { -- Mason
           'VonHeikemen/lsp-zero.nvim',
